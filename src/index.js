0f8321cb6252f5b602c07b5fc3a7453548c5bc04
priyanga_pas <apriyangasingh@gmail.com>
2023-09-04 01:09:30 -0700

    theam

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-//import './index.css';
+
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import "./superhero.css";
 import 'bootstrap/dist/js/bootstrap.bundle';
 import App from './pages/App/App';
 import { PolicyContextProvider } from './context/PolicyContext';
